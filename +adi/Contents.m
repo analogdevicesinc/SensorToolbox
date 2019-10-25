725ae33fdfba3e17b7956166305d246b638e608a
Travis F. Collins <travis.collins@analog.com>
2019-10-25 10:01:18 -0700

    Update all version and doc
    Signed-off-by: Travis F. Collins <travis.collins@analog.com>

---
+++ +adi/Contents.m
@@ -0,0 +1,13 @@
+% Analog Devices, Inc. Sensor Toolbox
+% Version 19.2.1 (R2019b)
+%
+% ==== Table of Contents (TOC) ====
+%
+% Parts
+% -----------------------
+%   <a href="matlab:help adi.ADIS16375      ">ADIS16375</a>         - IMU
+%   <a href="matlab:help adi.ADIS16460      ">ADIS16460</a>         - IMU
+%   <a href="matlab:help adi.ADIS16480      ">ADIS16480</a>         - IMU
+%   <a href="matlab:help adi.ADIS16485      ">ADIS16485</a>         - IMU
+%   <a href="matlab:help adi.ADIS16488      ">ADIS16488</a>         - IMU
+
