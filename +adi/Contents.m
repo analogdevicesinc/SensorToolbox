30cc8b52254d592766ba4fb6461fdf79ddc8ad37
Travis F. Collins <travis.collins@analog.com>
2021-02-04 10:57:56 -0800

    Update contents files
    Signed-off-by: Travis F. Collins <travis.collins@analog.com>

--- +adi/Contents.m
+++ +adi/Contents.m
@@ -10,4 +10,7 @@
 %   <a href="matlab:help adi.ADIS16480      ">ADIS16480</a>         - IMU
 %   <a href="matlab:help adi.ADIS16485      ">ADIS16485</a>         - IMU
 %   <a href="matlab:help adi.ADIS16488      ">ADIS16488</a>         - IMU
-
+%
+% Kits
+% -----------------------
+%   <a href="matlab:help adi.CN0532         ">CN0532 Sensor for CN0549 Kit</a>         - CBM Kit with ADXL1002
